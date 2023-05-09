a548f9a7a8755ed9e4e906d215de644ad8bd7d11
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2023-05-10 00:16:05 +0900

    fix

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, PostController],
-  providers: [AppService, PostService, PrismaService],
+  controllers: [PostController],
+  providers: [PostService, PrismaService],
 })
 export class AppModule {}
