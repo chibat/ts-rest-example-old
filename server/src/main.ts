e892d7a4058570d7ed3235c1d571d47c46e18f4b
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2023-05-10 19:19:52 +0900

    openapi

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { apiBlog } from "contract";
+import { generateOpenApi } from "@ts-rest/open-api";
+import { SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = generateOpenApi(apiBlog, {
+    info: {
+      title: "Posts API",
+      version: "1.0.0",
+    },
+  }, {
+    setOperationId: true,
+  });
+
+  SwaggerModule.setup("api", app, document);
+  //                   ^ Path for swagger
+
   await app.listen(3000);
 }
 bootstrap();
+
+// swagger-ui: http://localhost:3000/api
+// spec: http://localhost:3000/api-json
